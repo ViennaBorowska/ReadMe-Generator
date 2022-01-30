a5411ada9eb3b525c7ef5fe61a61c8b1f14f6662
Vienna Borowska <vienna.borowska@hotmail.com>
2022-01-30 18:37:57 +0000

    removed dotenv, couldn't get it working. Changed MySQL password instead so is now unique to MySQL.

--- index.js
+++ index.js
@@ -1,119 +1,119 @@
 //Packages & imports
-const inquirer = require("inquirer");
-const fs = require("fs");
-const path = require("path");
-const generateMarkdown = require("./utils/generateMarkdown");
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 //Questions Array
 const questions = [
   {
-    type: "input",
-    name: "title",
-    message: "What is the title of your project?",
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
     validate: (userInput) => {
       if (userInput) {
         return true;
       } else {
-        console.log("Please enter your project title!");
+        console.log('Please enter your project title!');
         return false;
       }
     },
   },
   {
-    type: "input",
-    name: "description",
-    message: "Please write a short description of your project.",
+    type: 'input',
+    name: 'description',
+    message: 'Please write a short description of your project.',
     validate: (userInput) => {
       if (userInput) {
         return true;
       } else {
-        console.log("Please enter a description!");
+        console.log('Please enter a description!');
         return false;
       }
     },
   },
   {
-    type: "input",
-    name: "install",
-    message: "What are the steps required to install your project?",
+    type: 'input',
+    name: 'install',
+    message: 'What are the steps required to install your project?',
     validate: (userInput) => {
       if (userInput) {
         return true;
       } else {
-        console.log("Please enter installation steps!");
+        console.log('Please enter installation steps!');
         return false;
       }
     },
   },
   {
-    type: "input",
-    name: "usage",
-    message: "How do you use this app?",
+    type: 'input',
+    name: 'usage',
+    message: 'How do you use this app?',
     validate: (userInput) => {
       if (userInput) {
         return true;
       } else {
-        console.log("Please enter a usage description!");
+        console.log('Please enter a usage description!');
         return false;
       }
     },
   },
   {
-    type: "list",
-    name: "license",
-    message: "What kind of license should your project have?",
+    type: 'list',
+    name: 'license',
+    message: 'What kind of license should your project have?',
     choices: [
-      "MIT",
-      "GNU",
-      "APACHE 2.0",
-      "GPL 3.0",
-      "BSD 3",
-      "ISC",
-      "NO LICENSE",
+      'MIT',
+      'GNU',
+      'APACHE 2.0',
+      'GPL 3.0',
+      'BSD 3',
+      'ISC',
+      'NO LICENSE',
     ],
-    default: ["MIT"],
+    default: ['MIT'],
     validate: (userInput) => {
       if (userInput) {
         return true;
       } else {
-        console.log("Please choose a license!");
+        console.log('Please choose a license!');
         return false;
       }
     },
   },
   {
-    type: "input",
-    name: "contributors",
-    message: "What does the user need to know about contributing to the repo?",
+    type: 'input',
+    name: 'contributors',
+    message: 'What does the user need to know about contributing to the repo?',
   },
   {
-    type: "input",
-    name: "test",
-    message: "What command should be run to run tests?",
-    default: "npm test",
+    type: 'input',
+    name: 'test',
+    message: 'What command should be run to run tests?',
+    default: 'npm test',
   },
   {
-    type: "input",
-    name: "github",
-    message: "What is your GitHub username?",
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?',
     validate: (userInput) => {
       if (userInput) {
         return true;
       } else {
-        console.log("Please enter your GitHub username!");
+        console.log('Please enter your GitHub username!');
         return false;
       }
     },
   },
   {
-    type: "input",
-    name: "email",
-    message: "What is your email address?",
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
     validate: (userInput) => {
       if (userInput) {
         return true;
       } else {
-        console.log("Please enter your email address!");
+        console.log('Please enter your email address!');
         return false;
       }
     },
@@ -124,11 +124,11 @@ const questions = [
 function writeToFile(fileName, data) {
   return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
-//Function to build ReadMe with user imput & confirm in terminal
+//Function to build HTML with user imput & confirm in terminal
 function init() {
   inquirer.prompt(questions).then((answers) => {
-    console.log("Successfully created README.md!");
-    writeToFile("My-README/README.md", generateMarkdown({ ...answers }));
+    console.log('Successfully created README.md!');
+    writeToFile('My-README/README.md', generateMarkdown({ ...answers }));
   });
 }
 
